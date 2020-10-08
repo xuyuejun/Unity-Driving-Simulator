50d8374c84f8448798adf41f274f6471c3a6519d
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-10-08 15:42:25 +0900

    Add Car Controller

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(InputManager))]
+
+public class CarController : MonoBehaviour
+{
+    internal enum driveType
+	{
+		FrontWheelDrive,
+		RearWheelDrive,
+		AllWheelDrive
+	}
+    public float topSpeed;                    // 最高速度
+    public float accleration;                 // 加速度
+    public float boostForce = 1000;           // boost
+    public Transform[] wheelMesh;             // 车轮模型
+    public WheelCollider[] wheelColliders;    // 车轮碰撞器
+
+	[HideInInspector]public float fwdInput, backInput, horizontalInput;
+
+
+    private InputManager im;                  // 输入管理 import manager
+    private Rigidbody car;                    // 汽车刚体
+
+    void Awake ()
+	{
+		car = GetComponent<Rigidbody> ();
+		im = GetComponent<InputManager> ();
+	}
+
+    void FixedUpdate()
+    {
+        // fwdInput = im.forward;
+		// backInput = im.backward;
+		// horizontalInput = im.Horizontal;
+        animateWheels ();
+    }
+
+    void animateWheels ()
+    {
+        Vector3 wheelPosition = Vector3.zero;
+		Quaternion wheelRotation = Quaternion.identity;
+
+		for (int i = 0; i < 4; i++) {
+			wheelColliders [i].GetWorldPose (out wheelPosition, out wheelRotation);
+			wheelMesh [i].position = wheelPosition;
+			wheelMesh [i].rotation = wheelRotation;
+		}
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal enum playerType
+{
+	keyboard,
+	controller
+}
+
+public class InputManager : MonoBehaviour
+{
+	[SerializeField]private playerType _playerType;
+
+
+	public float Horizontal { get; set; }
+
+	public float forward{ get; set; }
+
+	public float backward{ get; set; }
+
+	void Start ()
+	{
+		
+	}
+
+
+	void Update ()
+	{
+		if (_playerType == playerType.keyboard) {
+			Horizontal = Input.GetAxis ("Horizontal");
+			forward = (Input.GetAxis ("Vertical") > 0) ? Input.GetAxis ("Vertical") : 0;
+			backward = (Input.GetAxis ("Vertical") < 0) ? Input.GetAxis ("Vertical") : 0;
+		} else if (_playerType == playerType.controller) {
+			// do nothing.
+		} else {
+			// do nothing.
+			// AI script provides input.
+		}
+	}
+
+	public int ReturnPlayerType ()
+	{
+		if (_playerType == playerType.keyboard)
+			return 2;
+		else if (_playerType == playerType.controller)
+			return 1;
+		else
+			return 0;
+	}
+}
