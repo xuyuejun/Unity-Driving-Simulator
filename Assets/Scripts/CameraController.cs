f954a49fa4a66edb724a673f8631d9aa6db889d2
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-10-13 19:56:19 +0900

    Update Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public KeyCode switchViewKey = KeyCode.Space;
+    public float smoothing = 6f;
+    public Transform FirstPersonTarget;
+    public Transform ThirdPersonTarget;
+    public Transform lookAtTarget;
+    bool m_SHowingFirstPersonView;
+    void FixedUpdate()
+    {
+        UpdateCamera ();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown (switchViewKey))
+        {
+            m_SHowingFirstPersonView = !m_SHowingFirstPersonView;
+        }    
+    }
+    private void UpdateCamera ()
+    {
+        if (m_SHowingFirstPersonView)
+        {
+            transform.position = FirstPersonTarget.position;
+            transform.rotation = FirstPersonTarget.rotation;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, ThirdPersonTarget.position, Time.deltaTime * smoothing);
+            transform.LookAt(lookAtTarget);
+        }    
+    }
+}
