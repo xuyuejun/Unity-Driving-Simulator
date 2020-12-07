de270da4ebeb1e1bc13f360979279a7f7889153b
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-12-07 19:11:02 +0900

    Add MiniMap

---
+++ Assets/Scripts/MiniMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Transform car;
+    public Transform MiniMapCamera;
+    public float MapZoomHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MiniMapCamera.position = new Vector3(car.position.x,MapZoomHeight,car.position.z);
+        MiniMapCamera.rotation = Quaternion.Euler(90,car.eulerAngles.y,car.eulerAngles.z);
+    }
+}
