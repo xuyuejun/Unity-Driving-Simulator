40c620d2c5a7fd0343b115760a62ce7a61f14970
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-12-07 19:30:51 +0900

    add zoom function

--- Assets/Scripts/MiniMap.cs
+++ Assets/Scripts/MiniMap.cs
@@ -6,17 +6,22 @@ public class MiniMap : MonoBehaviour
 {
     public Transform car;
     public Transform MiniMapCamera;
-    public float MapZoomHeight;
-    // Start is called before the first frame update
-    void Start()
+    public float[] Zooms;
+    public int zoom = 2;
+    void OnZoomChange()
     {
-        
+        if (zoom == 0)
+        {
+            zoom = Zooms.Length - 1;
+        }
+        else
+        {
+            zoom --;
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        MiniMapCamera.position = new Vector3(car.position.x,MapZoomHeight,car.position.z);
-        MiniMapCamera.rotation = Quaternion.Euler(90,car.eulerAngles.y,car.eulerAngles.z);
+        MiniMapCamera.position = new Vector3(car.position.x, Zooms[zoom], car.position.z);
+        MiniMapCamera.rotation = Quaternion.Euler(90, car.eulerAngles.y, car.eulerAngles.z);
     }
 }
