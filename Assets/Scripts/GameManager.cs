fd7641d2b034d0fd56320eb1e147d5c992a7677d
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-12-14 17:27:41 +0900

    daily update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,51 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-
+    [Header("Input")]
+    public CarController car;
+    public CameraController mainCamera;
+    [Header("Location Status")]
+    public float Xcoordinate;
+    public float Ycoordinate;
+    public float Altitude;
+    [Header("Car Status")]
+    public float CarSpeed;
+    public float engineRPM;
+    public bool isBrake;
+    public float GearNum;
+    [Header("Main Camera Status")]
+    public int cameraStatus;
     void Start()
     {
-        
+
     }
     void FixedUpdate()
     {
-        
+        CarSpeed = car.CarSpeed;
+        GearNum = car.GearNum;
+        engineRPM = car.engineRPM;
+
+        Xcoordinate = car.transform.position.x + 600;
+        Ycoordinate = car.transform.position.z + 400;
+        Altitude = car.transform.position.y - 270;
+
+        debug();
+        // OnGUI();
     }
+    void debug()
+    {
+        Debug.Log("Test");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("CarSpeed: " + CarSpeed);
+        GUILayout.Label("engineRPM: " + engineRPM);
+        GUILayout.Label("GearNum: " + GearNum);
+
+        GUILayout.Label("Xcoordinate: " + Xcoordinate);
+        GUILayout.Label("Ycoordinate: " + Ycoordinate);
+        GUILayout.Label("Altitude" + Altitude);
+    }
+
 }
