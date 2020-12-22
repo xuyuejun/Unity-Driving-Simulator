516e89472256138e6050b4cb84709d8fafa66437
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-12-22 11:28:07 +0900

    Add Inside Dashboard

---
+++ Assets/Scripts/InsideDashboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InsideDashboard : MonoBehaviour
+{
+    public CarController car;
+    public Text speedText;
+    public float Speed;
+    // Update is called once per frame
+    void Update()
+    {
+        Speed = car.CarSpeed;
+        speedText.text = Speed.ToString("0");
+    }
+}
