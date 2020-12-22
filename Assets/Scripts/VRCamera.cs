c21219f9d3110c3d626ca6947eb795201740c5ca
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-12-22 10:35:07 +0900

    update vr camera

--- Assets/Scripts/VRCamera.cs
+++ Assets/Scripts/VRCamera.cs
@@ -9,13 +9,5 @@ public class VRCamera : MonoBehaviour
     void Start()
     {
         transform.localPosition = new Vector3(transform.localPosition.x - VR.localPosition.x, transform.localPosition.y - VR.localPosition.y, transform.localPosition.z - VR.localPosition.z);
-
-        // transform.position = new Vector3(transform.position.x - VR.position.x, transform.position.y - VR.position.y, transform.position.z - VR.position.z);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
